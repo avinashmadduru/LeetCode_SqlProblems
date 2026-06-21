e99aa15289152ef40363fe7c184685e8ced19805
Avinash Madduru <82103290+avinashmadduru@users.noreply.github.com>
2026-06-21 14:16:17 +0530

    Time: 1391 ms (75.98%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,9 +1,9 @@
-SELECT name
-FROM (SELECT sales_id
-FROM Orders 
-JOIN Company 
-    ON Orders.com_id = Company.com_id
-    WHERE Company.name LIKE 'RED') AS cmp
-RIGHT JOIN SalesPerson
-    ON cmp.sales_id = SalesPerson.sales_id
-    WHERE cmp.sales_id IS NULL;
+SELECT name 
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT sales_id 
+    FROM Orders 
+    JOIN Company 
+        ON Orders.com_id = Company.com_id
+        WHERE Company.name LIKE 'RED'
+);
