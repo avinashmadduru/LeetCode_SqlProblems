0a443d22c8c436ef0cb648f705124156441b2d94
Avinash Madduru <82103290+avinashmadduru@users.noreply.github.com>
2026-06-26 09:33:28 +0530

    Time: 1323 ms (23.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,6 @@
+SELECT p.product_id, p.product_name 
+FROM Sales s 
+JOIN Product p 
+    ON s.product_id = p.product_id 
+GROUP BY p.product_id, p.product_name
+HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31';
