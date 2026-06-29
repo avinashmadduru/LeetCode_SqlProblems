a7af8123aac0989352bd5938d160f88d31621373
Avinash Madduru <82103290+avinashmadduru@users.noreply.github.com>
2026-06-29 15:09:50 +0530

    Time: 632 ms (26.93%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,15 +1,12 @@
-WITH cte AS (
 SELECT e.employee_id
 FROM employees e
 LEFT JOIN salaries s
     ON e.employee_id = s.employee_id
-WHERE name IS NULL OR salary IS NULL
+WHERE salary IS NULL
 UNION
 SELECT s.employee_id
 FROM employees e
 RIGHT JOIN salaries s
     ON e.employee_id = s.employee_id
-WHERE name IS NULL OR salary IS NULL
-)SELECT * 
-FROM cte 
+WHERE name IS NULL
 ORDER BY employee_id;
